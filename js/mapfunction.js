d5962cb9d2d980cb24b5542c2d39615b033b102f
Eunice Dhivya <divi.seed@gmail.com>
2020-06-24 19:09:27 +0530

    add tooltips and button function

--- js/mapfunction.js
+++ js/mapfunction.js
@@ -1,10 +1,12 @@
-function mapfunction(selector){
+function mapfunction(selector, month){
 
     var margin = {top: 0, left: 0, right: 0, bottom: 0},
     scale = 1000,
     height = 500 - margin.top - margin.bottom,
     width = 800 - margin.left - margin.right;
     mapurl = "js/us_states.json"
+
+    d3.select(selector).html(null)
 
     var svg = d3.select(selector)
         .append("svg")
@@ -14,9 +16,8 @@ function mapfunction(selector){
 
     // var g = svg.append("g")
 
-    var colorScale = d3.scaleThreshold()
-    .domain([0, 100, 500, 1000, 5000, 10000, 20000, 40000, 60000])
-    .range(["#f7fcfd", "#e0ecf4", "#bfd3e6", "#9ebcda", "#8c96c6", "#8c6bb1", "#88419d", "#810f7c", "#4d004b"] );
+    var colorScale = d3.scaleQuantize()
+    .range(["#fcfbfd","#f1eff6","#e2e1ef","#cecee5","#b6b5d8","#9e9bc9","#8782bc","#7363ac","#61409b","#501f8c","#3f007d"]);
 
     var colorScaleParty = {
         "R": "#ff6b6b",
@@ -37,6 +38,37 @@ function mapfunction(selector){
         });
     }
 
+    var tool_tip_map = d3.tip()
+    .attr("class", "d3-tip")
+    .offset([20, 0])
+    .html(function(d){
+        var fd = _.filter(coviddata, function(obj){
+            return obj["State"] === d.properties.name
+        })
+        // return d.properties.name + ":" + fd[0]["Party"];
+        return d.properties.name;
+    });
+
+    svg.call(tool_tip_map);
+    
+    var tool_tip_circle = d3.tip()
+    .attr("class", "d3-tip")
+    .offset([0, 0])
+    .html(function(d){
+        var fd = _.filter(coviddata, function(obj){
+            return obj["State"] === d["name"]
+        })
+        if(fd[0] !== undefined){
+            // console.log("fd", fd[0]);
+            return d["name"]+": "+fd[0][month];
+        }else{
+            return d["name"]+": NA";
+            // console.log("fd", "Data not available");
+        } 
+    });
+
+    svg.call(tool_tip_circle);
+
     
 
     d3.json(mapurl, function(mapdata){
@@ -51,11 +83,12 @@ function mapfunction(selector){
         var valueExtent = d3.extent(coviddata, function(obj) {
             // console.log(obj["June*"]);
             
-            return obj["June*"]; 
+            return obj[month]; 
         })
 
         console.log(valueExtent);
         
+        colorScale.domain(valueExtent)
         
         var size = d3.scaleSqrt()
             .domain(valueExtent)  // What's in the data
@@ -70,7 +103,6 @@ function mapfunction(selector){
             .attr("stroke", "#000000")
             .attr("stroke-width", 0.2)
             .attr("fill", function(d, i){
-                // console.log("d", d.properties.name);
 
                 var fd = _.filter(coviddata, function(obj){
                     return obj["State"] === d.properties.name
@@ -81,46 +113,51 @@ function mapfunction(selector){
                     return colorScaleParty[fd[0]["Party"]];
                 }else{
                     return "#FFFFFF";
-                    // console.log("fd", "Data not available");
                 }        
             })
+            .on('mouseover', tool_tip_map.show)
+            .on('mouseout', tool_tip_map.hide)
+
+
+    
 
         svg.selectAll("myCircles")
             .data(stateCentroid)
             .enter()
             .append("circle")
+            .attr("cx", function(d){ return d["latlong"][0];})
+            .attr("cy", function (d){ return d["latlong"][1]; })
+            .attr("r", 0)
+            .attr("fill-opacity", "85%")
+            .on('mouseover', tool_tip_circle.show)
+            .on('mouseout', tool_tip_circle.hide)
+            .transition().duration(1000)
             .attr("r", function(d){
 
                 var fd = _.filter(coviddata, function(obj){
                     return obj["State"] === d["name"]
                 })
-
-                // console.log(colorScale[fd[0]["June*"]]);
-
                 if(fd[0] !== undefined){
                     // console.log("fd", fd[0]);
-                    return size(fd[0]["June*"]);
+                    return size(fd[0][month]);
                 }else{
                     return 10;
                     // console.log("fd", "Data not available");
                 }  
 
             })
-            .attr("cx", function(d){
-                // console.log("cd", d);
-                
-                return d["latlong"][0]; 
-            })
-            .attr("cy", function (d){ return d["latlong"][1]; })
-            .attr("fill-opacity", "85%")
             .attr("fill", function(d){
+                
                 
                 var fd = _.filter(coviddata, function(obj){
                     return obj["State"] === d["name"]
                 })
 
+                
+
                 if(fd[0] !== undefined){
-                    return colorScale(fd[0]["June*"]);
+                    console.log(fd[0][month], colorScale(fd[0][month]));
+                    return colorScale(fd[0][month]);
                 }else{
                     return "#FFFFFF";
                 }  
